3af70cfd5aa44e8cb344ce5b121dba2fa6362db2
rajasekar2808 <91137652+rajasekar2808@users.noreply.github.com>
2022-10-06 17:40:35 +0200

    some changes to post process file

--- tools/post_process_paraview.py
+++ tools/post_process_paraview.py
@@ -20,13 +20,18 @@ import post_process_vtk_format as pp
 import numpy as np
 from mesh import simplexMesh
 
-mesh_file_name = r'D:\VBox shared folder\visco v1.6 - Copie (2)\msh\crack_tension_test.msh'  ## path to mesh file
+mesh_file_name = r'D:\VBox shared folder\visco v1.7\msh\beam_offset_notch_hole_fine.msh'  ## path to mesh file
 
-folder_path = r'D:\VBox shared folder\results'  ## path where all the results file in .npz format are stored
+ ## path where all the results file in .npz format are stored (each file corresponds to each time step)
+folder_path = r'D:\VBox shared folder\results_liger\beam_offset_notch_hole_260922\beam_offset_notch_hole_fine_PF_v0_26_09_22_contd_\results' 
+
+  ## path to the force displacment data
+fu_path = r'D:\VBox shared folder\results_liger\beam_offset_notch_hole_260922\beam_offset_notch_hole_fine_PF_v0_26_09_22_contd_\force_disp.npz'
 
 post_pr_file_name = 'output_post_proc'  ## final file name to open in paraview
 
 
+model_type= 'PF' ##enter 'LF' for lipf-field
 
 
 
@@ -45,7 +50,17 @@ post_pr_file_name = 'output_post_proc'  ## final file name to open in paraview
 
 
 
+
+##load mesh
 mesh_file = simplexMesh.readGMSH(mesh_file_name)
+
+
+## load data coressponding  to force displacement
+ld1 = np.load(fu_path)
+uimp = ld1['u']
+Fy = ld1['Fy']
+Fx = ld1['Fx']
+cl = ld1['cl']  ## crack length
 
 
 os.chdir(folder_path)
@@ -77,25 +92,34 @@ os.chdir(post_pr_dir)
 
 
 vtk_instance = pp.store_data_as_vtk(mesh_file)
-t = [] ## time
+t = [0] ## time
 name = []
-for it,fle_name in enumerate(srtd_files):
-    ld = np.load('../'+fle_name)
+nodal_data= None
+
+for it,fle_name in enumerate(srtd_files[:-1]):
+    print('\rPreparing file '+ str(it+1) +'/'+ str(len(srtd_files)), end = '\r')
+    ld = np.load('../'+fle_name, allow_pickle=1)
+    
     #t.append(ld['time'])
-    t.append(it)
+    t.append(t[-1]+ld['DT'][0])
     u = ld['u']   ## nodal dispalcements
     ux = u[:,0] ; uy = u[:,1];
     d = ld['d']   ## face damage
+    d_nodes = ld['d_nodes']
     stress = ld['stress']
     sx = stress[:,0];  sy = stress[:,1]; sxy = stress[:,2];
-    nodal_data = {'ux':ux, 'uy':uy}
-    face_data = {'damage':d, 'sx':sx,'sy':sy,'sxy':sxy}
+    if model_type =='PF':
+        nodal_data = {'ux':ux, 'uy':uy,'d_vertex':d_nodes} ## For PF
+    else:
+        nodal_data = {'ux':ux, 'uy':uy}   ## For LF
+    face_data = {'damage':d, 'sx':sx,'sy':sy,'sxy':sxy}  ## for PF face damage contains averaged values from vertices
+    global_data = {'uF':np.array([uimp[it], Fy[it]])}
     
     name.append('sim'+str(it) )
-    vtk_instance.save_vtk(path = name[it], point_data=nodal_data, cell_data= face_data)
+    vtk_instance.save_vtk(path = name[it], point_data=nodal_data, cell_data= face_data, field_data=global_data)
     
     
 
 vtk_instance.group_vtk(source =name, dest = post_pr_file_name, indices= t)
-
+print('\nFind the file in '+ os.getcwd() + "\\" + post_pr_file_name + '.pvd')
     
